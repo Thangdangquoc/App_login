8ecececa427b131834ac8620fb1cd8aa76824b8d
Devmenter11 <devmenter11@gmail.com>
2023-12-07 19:03:11 +0700

    lem

--- src/components/TableUser.js
+++ src/components/TableUser.js
@@ -1,38 +1,50 @@
-import { useEffect } from 'react';
-import Table from 'react-bootstrap/Table';
-const TableUsers = (props) => {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Table from "react-bootstrap/Table";
+import { fetchAllUser } from "../services/UserServices";
 
-    return (<>
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+const TableUsers = (props) => {
+  const [listUsers, setListUser] = useState([]);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+    if (res && res.data) {
+      setListUser(res.data);
+    }
+  };
+  console.log("rcheck: ", listUsers);
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>STT</th>
+            <th>Email</th>
+            <th>Firt Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((item, index) => {
+              return (
+                <tr key={`user-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.email}</td>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan={2}>Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
-            </tbody>
-        </Table>
-    </>)
-}
+              );
+            })}
+        </tbody>
+      </Table>
+    </>
+  );
+};
 
 export default TableUsers;
---
+++ src/services/UserServices.js
@@ -0,0 +1,21 @@
+import instance from "./customize-axios";
+import axios from "./customize-axios";
+
+const fetchAllUser = () => {
+  return axios.get("/api/users?page=1");
+};
+
+// Add a response interceptor
+instance.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response.data;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+export { fetchAllUser };
